c488d9bc24a1cf8e9e0467acbef01fac26547509
yvetteyuanqin <lilyoorainqy@gmail.com>
2018-02-16 15:56:06 -0800

    clear ms

--- part2/mandelbrot_ms.cc
+++ part2/mandelbrot_ms.cc
@@ -10,34 +10,6 @@
 int
 main (int argc, char* argv[])
 {
-    double minX = -2.1;
-    double maxX = 0.7;
-    double minY = -1.25;
-    double maxY = 1.25;
-    
-    int height, width;
-    if (argc == 3) {
-        height = atoi (argv[1]);
-        width = atoi (argv[2]);
-        assert (height > 0 && width > 0);
-    } else {
-        fprintf (stderr, "usage: %s <height> <width>\n", argv[0]);
-        fprintf (stderr, "where <height> and <width> are the dimensions of the image.\n");
-        return -1;
-    }
-    
-    double it = (maxY - minY)/height;
-    double jt = (maxX - minX)/width;
-    double x, y;
-    
-    
-    gil::rgb8_image_t img(height, width);
-    auto img_view = gil::view(img);
-    
-  /* Lucky you, you get to write MPI code */
-    int curr_rank;
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank); MPI_Comm_size (MPI_COMM_WORLD, &size);
     
     
 }
